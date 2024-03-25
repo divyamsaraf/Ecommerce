1a08dbb168e0d8d4b8a1804a4b04039ef2952108
divyamsaraf <divyam.saraf@gmail.com>
2024-03-24 17:40:08 -0700

    second commit

--- apps/cart/cart.py
+++ apps/cart/cart.py
@@ -24,7 +24,7 @@ class Cart(object):
             self.cart[str(p)]['product'] = Product.objects.get(pk=p)
 
         for item in self.cart.values():
-            item['total_price'] = int(item['price']) * int(item['quantity'])
+            item['total_price'] = float(item['price']) * int(item['quantity'])
 
             yield item
 
